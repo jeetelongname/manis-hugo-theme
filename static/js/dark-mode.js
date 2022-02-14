9dcfa5d0588a2dc060f76302a95038aea2f04095
Jeetaditya Chatterjee <jeetelongname@gmail.com>
2022-02-14 02:00:15 +0000

    fix theme for me
    introduce a params.prefix option and hack dark mode to work

--- static/js/dark-mode.js
+++ static/js/dark-mode.js
@@ -14,9 +14,9 @@ var savedTheme = localStorage.getItem("dark-mode-storage") || systemDefault;
 setTheme(savedTheme);
 
 toggle.addEventListener("click", () => {
-    if (toggle.src.endsWith("/img/moon.svg") ) {
+    if (toggle.src.endsWith("/img/moon.svg")) {
         setTheme("dark");
-    } else if (toggle.src.endsWith("/img/sun.svg") ) {
+    } else if (toggle.src.endsWith("/img/sun.svg")) {
         setTheme("light");
     }
 
@@ -27,9 +27,9 @@ function setTheme(mode) {
 
     if (mode === "dark") {
         darkTheme.disabled = false;
-        toggle.src = "/img/sun.svg";
+        toggle.src = "/blog/img/sun.svg";
     } else if (mode === "light") {
         darkTheme.disabled = true;
-        toggle.src = "/img/moon.svg";
+        toggle.src = "/blog/img/moon.svg";
     }
 }
